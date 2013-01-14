909657ec6b4df976a8a007f78e4cb103ea056e0f
scoiatael <czapl.luk@gmail.com>
2013-01-14 15:51:53 +0100

    Quick fix

--- tutorial04_colored_cube-dir/tutorial04.cpp
+++ tutorial04_colored_cube-dir/tutorial04.cpp
@@ -71,8 +71,8 @@ int main( void )
 
 
     glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);
-    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
-    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 2);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 1);
     glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Open a window and create its OpenGL context
